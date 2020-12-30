ac2c7dc3dae79dc57f9fcbde79296eb5f7f424bf
nuhour <nuhour@163.com>
2020-12-30 14:43:01 +0800

    java file fommater

--- src/DemoGit.java
+++ src/DemoGit.java
@@ -1,6 +1,6 @@
 class DemoGit{
 	
-	private static final String CN = "中国"
+    private static final String CN = "中国"
 	
     public static void main(String args[]) {
         System.out.println("Hello Git!!");
